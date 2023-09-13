a9f6bd59ec1398e84237550f2c9da63f5d2d3b8e
Manish Chaudhary <manishchaudhary941286@gmail.com>
2023-09-13 14:39:41 +0530

    Initial commit

---
+++ book-manager-application/DataAccess/ConnectionString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    abstract class DBString
+    {
+        public string dbStr = "Data Source=DESKTOP-6S5A5AL\\SQLEXPRESS;Initial Catalog=bookManager;Integrated Security=True";
+    }
+    
+}
---
+++ book-manager-application/windows-app-book-manager/ConnectionString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp1
+{
+    class ConnectionString
+{
+        public static readonly string dbstr = "Data Source=DESKTOP-6S5A5AL\\SQLEXPRESS;Initial Catalog=bookManager;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+}
+}
+
